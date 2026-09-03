7f0ce45427d6344d6794d2f9e9c3705ff7d81819
talacada <atalac@proton.me>
2026-09-03 07:49:24 +0200

    GameCostEnum tests

--- tests/Unit/Enum/GameCostEnumTest.php
+++ tests/Unit/Enum/GameCostEnumTest.php
@@ -4,9 +4,26 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Enum;
 
+use App\Enum\CurrencyEnum;
+use App\Enum\GameCostEnum;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class GameCostEnumTest extends TestCase
 {
-    // TODO: testChangeAppearanceReturnsCorrectAmountAndCurrency() - assert getAmount() returns 5 and getCurrency() returns CurrencyEnum::DIAMONDS for CHANGE_APPEARANCE
+    #[DataProvider('costDataProvider')]
+    public function testGameCostValues(GameCostEnum $cost, int $expectedAmount, CurrencyEnum $expectedCurrency): void
+    {
+        $this->assertSame($expectedAmount, $cost->getAmount());
+        $this->assertSame($expectedCurrency, $cost->getCurrency());
+    }
+
+    public static function costDataProvider(): iterable
+    {
+        yield 'change appearance' => [
+            GameCostEnum::CHANGE_APPEARANCE,
+            5,
+            CurrencyEnum::DIAMONDS,
+        ];
+    }
 }
